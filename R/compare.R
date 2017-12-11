c5efcc03f4b4d75a3eb5eaa5a6aea6206866b572
Will Lamb <lamb.wf@gmail.com>
2017-12-11 16:13:45 +0100

    new figs

--- R/compare.R
+++ R/compare.R
@@ -10,44 +10,60 @@ load(file = "Data/city_studies.RData")
 load(file = "Data/city_data.RData")
 
 #attempt a bind
-ctop <- left_join(ctop,cdat, by = c("vars" = "city"))
-
-studies_vs_pop <- ctop %>%
-  filter(n>5) %>%
-  mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
-  ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=pop),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(pop,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$pop,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city population (bar)") +
-  xlab("Cities, ordered by article count")
-
-studies_vs_gdp <- ctop %>%
-  filter(n>5) %>%
-  mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
-  ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=gdp/pop),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(gdp/pop,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$gdp/ctop$pop,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city GDP/POP (bar)") +
-  xlab("Cities, ordered by article count")
-
-studies_vs_co2 <- ctop %>%
+ctop <- left_join(ctop,cdat, by = c("vars" = "city")) %>%
+  select(vars,n,pop,gdp,co2) %>%
   filter(n>9) %>%
-  mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
-  ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=co2/pop),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(co2/pop,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$co2/ctop$pop,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city CO2/POP (bar)") +
-  xlab("Cities, ordered by article count")
+  mutate(n=scale(n,center=min(n),scale=max(n)-min(n)))
 
-ggsave(file = "Plots/City_studies_pop.pdf",plot = studies_vs_pop)
-ggsave(file = "Plots/City_studies_gdp.pdf",plot = studies_vs_gdp)
-ggsave(file = "Plots/City_studies_co2.pdf",plot = studies_vs_co2)
+st_bt_pop <-
+  ctop %>%
+  mutate(right="articles",left="pop") %>%
+  ggplot(.) +
+  geom_bar(aes(x=reorder(vars,-n),y=pop,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(pop,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$pop,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(pop="#4292c6"),labels="Population") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("Total Population (bottom-up urban data)")
+
+st_bt_gdp <-
+  ctop %>%
+  mutate(right="articles",left="gdp") %>%
+  ggplot(.) +
+  geom_bar(aes(x=reorder(vars,-n),y=gdp,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(gdp,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$gdp,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(gdp="#4292c6"),labels="GDP") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("Total GDP (bottom-up urban data)")
+
+st_bt_co2 <-
+  ctop %>%
+  mutate(right="articles",left="co2") %>%
+  ggplot(.) +
+  geom_bar(aes(x=reorder(vars,-n),y=co2,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(co2,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$co2,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(co2="#4292c6"),labels="CO2") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("Total CO2 (bottom-up urban data)")
+
+plot(st_bt_pop)
+plot(st_bt_gdp)
+plot(st_bt_co2)
+
+ggsave(file = "Plots/cities_studies_pop_bt.pdf",plot = st_bt_pop)
+ggsave(file = "Plots/cities_studies_gdp_bt.pdf",plot = st_bt_gdp)
+ggsave(file = "Plots/cities_studies_co2_bt.pdf",plot = st_bt_co2)
 
 ###################
 
@@ -56,40 +72,58 @@ load(file = "Data/city_studies.RData")
 load(file = "Data/city_data_pnas.RData")
 
 #attempt a bind
-ctop <- left_join(ctop,cpnas, by = c("vars" = "cities"))
-
-#population
-ctop %>%
-  filter(n>2) %>%
+ctop <- left_join(ctop,cpnas, by = c("vars" = "cities")) %>%
+  select(vars,n,pop=population,gdp_pc=gdp_per_cap,co2_int=emission_intensity_2009)%>%
+  filter(n>9) %>%
   mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
-  ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=population),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(population,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$population,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city population (bar)") +
-  xlab("Cities, ordered by article count")
+  mutate(right="articles",left="pop")
 
-#gdp
-ctop %>%
-  filter(n>2) %>%
-  mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
+# population
+st_gea_pop <-
+  ctop %>%
   ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=gdp_per_cap),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(gdp_per_cap,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$gdp_per_cap,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city gdp_per_cap (bar)") +
-  xlab("Cities, ordered by article count")
+  geom_bar(aes(x=reorder(vars,-n),y=pop,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(pop,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$pop,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(pop="#4292c6"),labels="Population") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("Total Population (GEA data)")
 
-#emission_intensity_2009
-ctop %>%
-  filter(n>2) %>%
-  mutate(n=scale(n,center=min(n),scale=max(n)-min(n))) %>%
+# gdp_pc
+st_gea_gdppc <-
+  ctop %>%
   ggplot(.) +
-  geom_bar(aes(x=reorder(vars,-n),y=emission_intensity_2009),stat="identity") +
-  geom_line(aes(x=reorder(vars,-n),y=n*max(emission_intensity_2009,na.rm=TRUE)),stat="identity",group=1) +
-  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$emission_intensity_2009,na.rm=TRUE))) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  ggtitle("Article count (line) vs. city gdp_per_cap (bar)") +
-  xlab("Cities, ordered by article count")
+  geom_bar(aes(x=reorder(vars,-n),y=gdp_pc,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(gdp_pc,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$gdp_pc,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(pop="#4292c6"),labels="GDP PC") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("GDP per capita (GEA data)")
+
+# co2_int
+st_gea_co2int <-
+  ctop %>%
+  ggplot(.) +
+  geom_bar(aes(x=reorder(vars,-n),y=co2_int,fill=left),stat="identity") +
+  geom_line(aes(x=reorder(vars,-n),y=n*max(co2_int,na.rm=TRUE),color=right),stat="identity",group=1) +
+  scale_y_continuous(sec.axis = sec_axis(~./max(ctop$co2_int,na.rm=TRUE))) +
+  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+        legend.position=c(1,1),legend.justification=c(1,1),legend.title=element_blank()) +
+  scale_fill_manual(values=c(pop="#4292c6"),labels="CO2 Intensity") +
+  scale_color_manual(values=c(articles="#e6550d"),labels="Articles (normalised 10-147)")  +
+  xlab("Cities, ordered by article count")   +
+  ylab("CO2 Intensity (GEA data)")
+
+plot(st_gea_pop)
+plot(st_gea_gdppc)
+plot(st_gea_co2int)
+
+ggsave(file = "Plots/cities_studies_pop_gea.pdf",plot = st_gea_pop)
+ggsave(file = "Plots/cities_studies_gdppc_gea.pdf",plot = st_gea_gdppc)
+ggsave(file = "Plots/cities_studies_co2int_gea.pdf",plot = st_gea_co2int)
