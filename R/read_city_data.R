dcb48e9d9503e4e56bb197a4421e6b43082d7d27
Will Lamb <lamb.wf@gmail.com>
2018-02-15 17:20:46 +0100

    asfdqer

--- R/read_city_data.R
+++ R/read_city_data.R
@@ -1,9 +1,5 @@
 rm(list = ls())
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(magrittr)
-library(reshape2)
+library(tidyverse)
 library(svglite)
 library(xlsx)
 
@@ -16,16 +12,17 @@ simpleCap <- function(x) {
 ############### PNAS/GEA data ############### 
 
 data_GEA <- read.xlsx(file="Data/city PNAS data.xlsx",sheetName="gea_data",encoding="UTF-8") %>%
-  select(cities,total_final_consumption_per_capita,country,population,gdp_per_cap,emission_intensity_2009)
-data_GEA <- data_GEA[!is.na(data_GEA$cities),]
+  select(cities,total_final_consumption_per_capita,country,population,gdp_per_cap,emission_intensity_2009) %>%
+  rename(City=cities)
+data_GEA <- data_GEA[!is.na(data_GEA$City),]
 
 
 ############### K Seto data ###############
 
 data_Seto <- read.xlsx(file="Data/SI.2 - Moran Spatial Footprints Data Appendix.xlsx",sheetName = "S.2.3a - Top 500 Cities",
                    encoding="UTF-8") %>%
-  rename(cities="Urban.Cluster",co2pc="Footprint.cap..t.CO2.",co2="Footprint..t.CO2.",pop="Population") %>%
-  select(cities,Country,co2pc,co2,pop)
+  rename(City="Urban.Cluster",co2pc="Footprint.cap..t.CO2.",co2="Footprint..t.CO2.",pop="Population") %>%
+  select(City,Country,co2pc,co2,pop)
 
 
 ############### UN City POP Data ############### 
@@ -51,19 +48,27 @@ data_UN <- data_UN %>%
 
 # Minor edits to city names
 data_UN$City <- gsub("Washington","Washington, D.C.", data_UN$City)
+data_UN$City <- gsub("New York","New York City", data_UN$City)
 data_UN$City <- gsub("Mexico, Ciudad De","Mexico City", data_UN$City)
+
+data_UN <- data_UN %>%
+  rename(UN_year=Source.Year,UN_type=City.type,UN_pop=Value)
+
 
 ############### OECD Income data ############### 
 
 data_OECD <- read.xlsx('Data/OECD city income.xlsx',encoding="UTF-8",sheetName = "Sheet1") %>%
-  rename(cities="Metropolitan.areas",year="Year",unit="Unit",value="Value") %>%
-  select(cities,year,unit,value)
+  rename(City="Metropolitan.areas",year="Year",unit="Unit",value="Value") %>%
+  select(City,year,unit,value)
+
+data_OECD$City <- gsub("Washington","Washington, D.C.", data_OECD$City)
+data_OECD$City <- gsub("New York","New York City", data_OECD$City)
 
 ############### Aggregated Income data ###############
 
 data_income <- data_GEA %>%
   rename(GEA_income="gdp_per_cap") %>%
-  select(cities,GEA_income)
+  select(City,GEA_income)
 
 data_income <- full_join(data_income,data_OECD) %>%
   rename(OECD_income="value") %>%
@@ -83,13 +88,38 @@ data_income <- full_join(data_income,data_OECD) %>%
 
 data_income <- data_income %>%
   gather(gdp,value,GEA_income,OECD_income) %>%
-  group_by(cities) %>%
+  group_by(City) %>%
   filter(is.na(value)==FALSE) %>%
   filter(row_number()==1)
 
+
+############### UN Urban Projection Data ###############
+
+data_proj <- read.xlsx(file="Data/WUP2014-F17a-City_Size_Class.xls",sheetName="DATA",encoding="UTF-8",startRow=17,header=TRUE)
+
+data_proj <- data_proj %>%
+  rename(area='Major.area..region..country.or.area',size="Size.class.of.urban.settlement",size_code="Size.class.code",type="Type.of.data") %>%
+  select(-c(Index,Country.Code))
+
+names(data_proj) <- gsub("X","",names(data_proj))
+
+data_proj <- data_proj %>%
+  gather(year,value,"1950":"2030") %>%
+  mutate(year=as.numeric(year)) %>%
+  arrange(size_code)
+
+levels <- unique(data_proj$size)
+data_proj$size <- factor(data_proj$size, levels = levels)
+
+
+############### GEONames Database ############### 
+
+# data_geo <- read.xlsx(file="Data/cities15000.xlsx",sheetName="Sheet1",encoding="UTF-8",header=TRUE) 
+# save(data_geo,file="Data/geo_data.RData")
+
 ############### save ###############
 
-save(data_GEA,data_Seto,data_UN,data_OECD,data_income,file="Data/city_data.RData")
+save(data_GEA,data_Seto,data_UN,data_OECD,data_income,data_proj,file="Data/city_data.RData") 
 
 ############### Funny spatial data ############### 
 
--- R/read_topic_data.R
+++ R/read_topic_data.R
@@ -1,115 +1,122 @@
 rm(list = ls())
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-library(magrittr)
-library(reshape2)
+library(tidyverse)
 library(svglite)
 library(xlsx)
 
 ctab <-
   read.csv("Data/cities_places_topics.csv",sep=",",header=TRUE, encoding="UTF-8") %>%
   subset(select=-c(X)) %>%
-  select(cities,year,title,abstract,doi,authors,everything())
+  select(cities,countries,year,title,abstract,doi,authors,everything()) %>%
+  rename(geo_city=cities,geo_country=countries,geo_pop=population)
 
 
 ############### duplicate rows where there is more than one city ###############
 
+
 for (i in 1:length(ctab[,1])) {
-  temp <- as.character(ctab$cities[i])
-  if(grepl(';',temp)=="TRUE") {
-    temp_cities <- strsplit(temp,';')
+  temp_cities <- as.character(ctab$geo_city[i])
+  temp_countries <- as.character(ctab$geo_country[i])
+  temp_pop <- as.character(ctab$geo_pop[i])
+  if(grepl(';',temp_cities)=="TRUE") {
+    temp_cities <- strsplit(temp_cities,';')
+    temp_countries <- strsplit(temp_countries,';')
+    temp_pop <- strsplit(temp_pop,';')
     for (j in 1:length(temp_cities[[1]])) {
       rowtobind <- ctab[i,]
-      rowtobind$cities <- temp_cities[[1]][j]
+      rowtobind$geo_city <- temp_cities[[1]][j]
+      rowtobind$geo_country <- temp_countries[[1]][j]
+      rowtobind$geo_pop <- temp_pop[[1]][j]
       ctab <- rbind(ctab,rowtobind)
     }
   }
 }
-ctab <- ctab[!(grepl(';',ctab$cities)),] %>%
-  mutate(cities=trimws(cities))
+
+ctab <- ctab[!(grepl(';',ctab$geo_city)),] %>%
+  mutate(geo_city=trimws(geo_city))
+ctab <- ctab[!(grepl(';',ctab$geo_country)),] %>%
+  mutate(geo_country=trimws(geo_country))
+ctab <- ctab[!(grepl(';',ctab$geo_pop)),] %>%
+  mutate(geo_pop=trimws(geo_pop))
 
 
-############### Extract country names ############### 
+############### Annoyances ###############
 
-# Get a list of country names and ISOs
-isos <- read.xlsx(file="C:\\Users\\lamw\\Google Drive\\Work\\Code\\MATLAB\\Handy code\\ISOcodes.xls",
-                  sheetName="3 letter codes",colIndex=7:8,startRow=3,header=FALSE,encoding="UTF-8") %>%
-  rename(countries=X7,isos=X8) %>%
-  filter(!countries %in% c("US","World","Jersey"),!isos %in% c("ZZZZ","ZZZA"))
 
-ctab$country <-NA
-for (i in 1:length(ctab$abstract)) {
-  for (j in 1:length(isos$countries)) {
-    if (grepl(isos$countries[j],ctab$abstract[i],ignore.case=TRUE)==TRUE)
-      ctab$country[i] <- paste0(ctab$country[i],",",isos$isos[j])
-  }
-}
-
-ctab$country <- gsub("NA,","",ctab$country)
 ctab <- ctab %>%
-  select("cities","country",everything())
+  mutate(geo_country=ifelse(grepl("Newcastle upon Tyne",abstract) & geo_city=="Newcastle","GBR",geo_country)) %>%
+  mutate(geo_country=ifelse(grepl("Australia",abstract) & geo_city=="Newcastle","AUS",geo_country))
 
-ctab$cities <- gsub("New York City","New York",ctab$cities)
 
-#### only keep country if it is the single one identified #### 
+############### Attach regions ############### 
+
+
+regions <- read.xlsx(file="C:\\Users\\lamw\\Google Drive\\Work\\Code\\MATLAB\\Data shop\\Region definitions\\regions.xls",sheetName = "Sheet1") %>%
+  select(ISO.Code,IAM10,UN6)
+
 ctab <- ctab %>%
-  mutate(country=ifelse(nchar(ctab$country)>4,NA,country))
+  left_join(regions,by=c("geo_country"="ISO.Code")) %>%
+  select(geo_city,geo_country,IAM10,UN6,everything())
 
 
+############### save and done ###############
+
+ctop <- ctab %>%
+     count(geo_city,sort=TRUE)
+
+save(ctab,file="Data/city_studies.RData")
 
 
 
 ############### Summary figures ############### 
 
-# Rank cities by no. papers
-ctop <- ctab %>%
-  count(vars=cities,sort=TRUE)
+# # Rank cities by no. papers
+# ctop <- ctab %>%
+#   count(vars=cities,sort=TRUE)
+# 
+# # Plot top cities in descending order
+# g_count <- ctop %>%
+#   filter(n>5) %>%
+#   ggplot(.,aes(x=reorder(vars,-n),y=n)) +
+#   geom_bar(stat="identity") +
+#   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+# 
+# # Set a topic threshold and count papers on each topic 
+# csum <- tidyr::gather(ctab,topic,value,Active.travel:e.Vehicles)
+# for (i in 1:length(csum[,1])) {
+#   if (csum$value[i]>0.005) {
+#     csum$value[i] <- 1
+#   }
+#   else {
+#     csum$value[i] <- 0
+#   }
+# }
+# 
+# csum <- csum %>%
+#   filter(value>0) %>%
+#   group_by(cities,topic) %>% 
+#   summarise_each(funs(sum),value)
+# 
+# z<-csum %>% 
+#   group_by(cities) %>% 
+#   summarise_each(funs(sum),value) %>% 
+#   arrange(desc(value))
+# 
+# z <- rename(z,total=value)
+# csum$city_ordered <- factor(csum$cities,levels=z$cities)
+# 
+# g_heat <- csum %>% 
+#   left_join(ctop,by=c("cities" = "vars")) %>%
+#   filter(n>9) %>%
+#   group_by(cities,topic) %>% 
+#   mutate(value = value/n) %>%
+#   ggplot(., aes(city_ordered,topic)) +
+#   geom_tile(aes(fill = value),colour = "white") +
+#   scale_fill_gradient(low = "white",high = "steelblue") +
+#   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+# 
+# ##
+# ggsave(file = "Plots/City_studies.pdf",plot = g_count)
+# ggsave(file = "Plots/City_topics.pdf",plot = g_heat)
 
-# Plot top cities in descending order
-g_count <- ctop %>%
-  filter(n>5) %>%
-  ggplot(.,aes(x=reorder(vars,-n),y=n)) +
-  geom_bar(stat="identity") +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
-# Set a topic threshold and count papers on each topic 
-csum <- tidyr::gather(ctab,topic,value,Active.travel:e.Vehicles)
-for (i in 1:length(csum[,1])) {
-  if (csum$value[i]>0.005) {
-    csum$value[i] <- 1
-  }
-  else {
-    csum$value[i] <- 0
-  }
-}
-
-csum <- csum %>%
-  filter(value>0) %>%
-  group_by(cities,topic) %>% 
-  summarise_each(funs(sum),value)
-
-z<-csum %>% 
-  group_by(cities) %>% 
-  summarise_each(funs(sum),value) %>% 
-  arrange(desc(value))
-
-z <- rename(z,total=value)
-csum$city_ordered <- factor(csum$cities,levels=z$cities)
-
-g_heat <- csum %>% 
-  left_join(ctop,by=c("cities" = "vars")) %>%
-  filter(n>9) %>%
-  group_by(cities,topic) %>% 
-  mutate(value = value/n) %>%
-  ggplot(., aes(city_ordered,topic)) +
-  geom_tile(aes(fill = value),colour = "white") +
-  scale_fill_gradient(low = "white",high = "steelblue") +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
-
-##
-ggsave(file = "Plots/City_studies.pdf",plot = g_count)
-ggsave(file = "Plots/City_topics.pdf",plot = g_heat)
-
-save(ctab,ctop,file="Data/city_studies.RData")
 
